ef19afc241079b76a33d9954866750f48b87ed08
Julian Grinblat <julian@dotcore.co.il>
2021-11-01 15:48:20 +0900

    Maintenance 2021

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,37 @@
-import babel from 'rollup-plugin-babel';
+import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 
 import pkg from './package.json';
 
 const input = 'src/index.js';
-const plugins = [babel()];
+const plugins = [babel({ babelHelpers: 'bundled' })];
 
 const name = 'ReduxPersistent';
 
 export default [{
   input,
-  output: { file: `dist/${pkg.name}.cjs.js`, format: 'cjs', exports: 'named' },
-  plugins,
-}, {
-  input,
-  output: { file: `dist/${pkg.name}.esm.js`, format: 'esm' },
+  output: {
+    file: `dist/${pkg.name}.cjs.js`,
+    format: 'cjs',
+    sourcemap: true,
+    exports: 'named',
+  },
   plugins,
 }, {
   input,
   output: {
-    file: `dist/${pkg.name}.umd.js`, format: 'umd', name, exports: 'named',
+    file: `dist/${pkg.name}.esm.js`,
+    format: 'esm',
+    sourcemap: true,
+  },
+  plugins,
+}, {
+  input,
+  output: {
+    file: `dist/${pkg.name}.umd.js`,
+    format: 'umd',
+    name,
+    exports: 'named',
   },
   plugins: [
     ...plugins,
