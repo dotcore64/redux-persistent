f93481c1fcbb001774dd2bc7dd9d175bc029442f
Julian Grinblat <julian@dotcore.co.il>
2026-07-14 04:01:07 +0900

    chore: add prettier (#303)

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const HYDRATE = Symbol('HYDRATE');
-const REMOVE = Symbol('REMOVE');
+const HYDRATE = Symbol("HYDRATE");
+const REMOVE = Symbol("REMOVE");
 
 export const hydrate = () => ({ [HYDRATE]: true });
 export const remove = (key) => ({ [REMOVE]: key });
@@ -18,52 +18,58 @@ const mapObject = (mapper, obj) => {
 
 let hydrating = false;
 
-export default (
-  selectors = {},
-  { storage = localStorage } = {},
-) => ({ dispatch, getState }) => (next) => (action) => {
-  if (action[HYDRATE]) {
-    hydrating = true;
+export default (selectors = {}, { storage = localStorage } = {}) =>
+  ({ dispatch, getState }) =>
+  (next) =>
+  (action) => {
+    if (action[HYDRATE]) {
+      hydrating = true;
+
+      return Promise.all(
+        Object.keys(selectors).map((key) =>
+          Promise.resolve(storage.getItem(key)) // Normalize into promise
+            .then((value) => {
+              const { load = noop } = selectors[key];
+
+              return load(value, { dispatch, getState });
+            }),
+        ),
+      ).then(() => {
+        hydrating = false;
+      });
+    }
+
+    if (action[REMOVE]) {
+      return Promise.resolve(storage.removeItem(action[REMOVE]));
+    }
+
+    if (hydrating) {
+      return next(action);
+    }
+
+    const prevState = getState();
+    const prevValues = mapObject(
+      ({ selector }) => selector(prevState),
+      selectors,
+    );
+
+    next(action);
+
+    const state = getState();
+
+    if (state === prevState) {
+      // selectors are pure functions of the state
+      return Promise.resolve();
+    }
 
     return Promise.all(
-      Object
-        .keys(selectors)
-        .map((key) => Promise.resolve(storage.getItem(key)) // Normalize into promise
-          .then((value) => {
-            const { load = noop } = selectors[key];
+      Object.keys(selectors).map((key) => {
+        const { selector } = selectors[key];
+        const value = selector(state);
 
-            return load(value, { dispatch, getState });
-          })),
-    ).then(() => { hydrating = false; });
-  }
-
-  if (action[REMOVE]) {
-    return Promise.resolve(storage.removeItem(action[REMOVE]));
-  }
-
-  if (hydrating) {
-    return next(action);
-  }
-
-  const prevState = getState();
-  const prevValues = mapObject(({ selector }) => selector(prevState), selectors);
-
-  next(action);
-
-  const state = getState();
-
-  if (state === prevState) { // selectors are pure functions of the state
-    return Promise.resolve();
-  }
-
-  return Promise.all(
-    Object.keys(selectors).map((key) => {
-      const { selector } = selectors[key];
-      const value = selector(state);
-
-      return (value === prevValues[key])
-        ? undefined
-        : storage.setItem(key, value);
-    }),
-  );
-};
+        return value === prevValues[key]
+          ? undefined
+          : storage.setItem(key, value);
+      }),
+    );
+  };
--- test/index.js
+++ test/index.js
@@ -1,32 +1,32 @@
-import { join } from 'node:path';
+import { join } from "node:path";
 
-import { stub } from 'sinon';
-import { expect } from 'chai';
-import { dirname } from 'dirname-filename-esm';
-import configureStore from 'redux-mock-store'; // eslint-disable-line import/no-named-as-default
-import { AsyncNodeStorage } from 'redux-persist-node-storage';
+import { stub } from "sinon";
+import { expect } from "chai";
+import { dirname } from "dirname-filename-esm";
+import configureStore from "redux-mock-store"; // eslint-disable-line import/no-named-as-default
+import { AsyncNodeStorage } from "redux-persist-node-storage";
 
 // https://github.com/import-js/eslint-plugin-import/issues/1649
 // eslint-disable-next-line import/no-unresolved
-import persistent, { remove, hydrate } from 'redux-persistent';
+import persistent, { remove, hydrate } from "redux-persistent";
 
-const getMockStore = (selectors, storage) => configureStore.default([
-  persistent(selectors, { storage }),
-]);
+const getMockStore = (selectors, storage) =>
+  configureStore.default([persistent(selectors, { storage })]);
 
-const getAsyncStorage = () => new AsyncNodeStorage(join(dirname(import.meta), 'storage'));
+const getAsyncStorage = () =>
+  new AsyncNodeStorage(join(dirname(import.meta), "storage"));
 
-describe('redux-persistent', () => {
-  describe('localStorage', () => {
+describe("redux-persistent", () => {
+  describe("localStorage", () => {
     beforeEach(() => localStorage.clear());
 
-    it('should save value to localstorage', () => {
-      const firstState = 'firstState';
-      const secondState = 'secondState';
+    it("should save value to localstorage", () => {
+      const firstState = "firstState";
+      const secondState = "secondState";
 
       const selector = stub();
-      selector.withArgs(firstState).returns('first');
-      selector.withArgs(secondState).returns('second');
+      selector.withArgs(firstState).returns("first");
+      selector.withArgs(secondState).returns("second");
 
       const getState = stub();
       getState.onFirstCall().returns(firstState);
@@ -39,19 +39,19 @@ describe('redux-persistent', () => {
       });
 
       const store = mockStore(getState);
-      store.dispatch({ type: 'myaction' });
+      store.dispatch({ type: "myaction" });
 
-      const saved = localStorage.getItem('foo');
-      expect(saved).to.equal('second');
+      const saved = localStorage.getItem("foo");
+      expect(saved).to.equal("second");
     });
 
-    it('should not save value to localstorage when the state does not change', () => {
-      const firstState = 'firstState';
-      const secondState = 'secondState';
+    it("should not save value to localstorage when the state does not change", () => {
+      const firstState = "firstState";
+      const secondState = "secondState";
 
       const selector = stub();
-      selector.withArgs(firstState).returns('first');
-      selector.withArgs(secondState).returns('second');
+      selector.withArgs(firstState).returns("first");
+      selector.withArgs(secondState).returns("second");
 
       const mockStore = getMockStore({
         foo: {
@@ -60,19 +60,19 @@ describe('redux-persistent', () => {
       });
 
       const store = mockStore(firstState);
-      store.dispatch({ type: 'myaction' });
+      store.dispatch({ type: "myaction" });
 
-      const saved = localStorage.getItem('foo');
+      const saved = localStorage.getItem("foo");
       expect(saved).to.equal(null);
     });
 
-    it('should not save value to localstorage when the selected state does not change', () => {
-      const firstState = 'firstState';
-      const secondState = 'secondState';
+    it("should not save value to localstorage when the selected state does not change", () => {
+      const firstState = "firstState";
+      const secondState = "secondState";
 
       const selector = stub();
-      selector.withArgs(firstState).returns('first');
-      selector.withArgs(secondState).returns('first');
+      selector.withArgs(firstState).returns("first");
+      selector.withArgs(secondState).returns("first");
 
       const getState = stub();
       getState.onFirstCall().returns(firstState);
@@ -85,123 +85,135 @@ describe('redux-persistent', () => {
       });
 
       const store = mockStore(getState);
-      store.dispatch({ type: 'myaction' });
+      store.dispatch({ type: "myaction" });
 
-      const saved = localStorage.getItem('foo');
+      const saved = localStorage.getItem("foo");
       expect(saved).to.equal(null);
     });
 
-    it('should remove item with remove action', () => {
-      localStorage.setItem('foo', 'bar');
-      expect(localStorage.getItem('foo')).to.equal('bar');
+    it("should remove item with remove action", () => {
+      localStorage.setItem("foo", "bar");
+      expect(localStorage.getItem("foo")).to.equal("bar");
 
       const mockStore = getMockStore({});
       const store = mockStore({});
-      store.dispatch(remove('foo'));
+      store.dispatch(remove("foo"));
 
-      expect(localStorage.getItem('foo')).to.equal(null);
+      expect(localStorage.getItem("foo")).to.equal(null);
     });
 
-    it('should hydrate the redux store', async () => {
-      localStorage.setItem('foo', 'myvalue');
+    it("should hydrate the redux store", async () => {
+      localStorage.setItem("foo", "myvalue");
 
       const mockStore = getMockStore({
         foo: {
-          load: ((value, { dispatch }) => {
+          load: (value, { dispatch }) => {
             dispatch({
-              type: 'hydrate',
+              type: "hydrate",
               value,
             });
-          }),
+          },
         },
       });
 
       const store = mockStore({});
       await store.dispatch(hydrate());
 
-      expect(store.getActions()).to.deep.equal([{
-        type: 'hydrate',
-        value: 'myvalue',
-      }]);
+      expect(store.getActions()).to.deep.equal([
+        {
+          type: "hydrate",
+          value: "myvalue",
+        },
+      ]);
     });
 
-    it('should create with empty selectors', async () => {
-      localStorage.setItem('foo', 'myvalue');
+    it("should create with empty selectors", async () => {
+      localStorage.setItem("foo", "myvalue");
 
       const mockStore = getMockStore();
 
       const store = mockStore({});
-      store.dispatch({ type: 'myaction' });
+      store.dispatch({ type: "myaction" });
 
-      expect(store.getActions()).to.deep.equal([{
-        type: 'myaction',
-      }]);
+      expect(store.getActions()).to.deep.equal([
+        {
+          type: "myaction",
+        },
+      ]);
     });
   });
 
-  describe('nodeStorage', () => {
-    it('should save value to storage', async () => {
+  describe("nodeStorage", () => {
+    it("should save value to storage", async () => {
       const storage = getAsyncStorage();
 
-      const firstState = 'firstState';
-      const secondState = 'secondState';
+      const firstState = "firstState";
+      const secondState = "secondState";
 
       const selector = stub();
-      selector.withArgs(firstState).returns('first');
-      selector.withArgs(secondState).returns('second');
+      selector.withArgs(firstState).returns("first");
+      selector.withArgs(secondState).returns("second");
 
       const getState = stub();
       getState.onFirstCall().returns(firstState);
       getState.onSecondCall().returns(secondState);
 
-      const mockStore = getMockStore({
-        foo: {
-          selector,
+      const mockStore = getMockStore(
+        {
+          foo: {
+            selector,
+          },
         },
-      }, storage);
+        storage,
+      );
 
       const store = mockStore(getState);
-      await store.dispatch({ type: 'myaction' });
+      await store.dispatch({ type: "myaction" });
 
-      const saved = await storage.getItem('foo');
-      expect(saved).to.equal('second');
+      const saved = await storage.getItem("foo");
+      expect(saved).to.equal("second");
     });
 
-    it('should remove item with remove action', async () => {
+    it("should remove item with remove action", async () => {
       const storage = getAsyncStorage();
 
-      await storage.setItem('foo', 'bar');
-      expect(await storage.getItem('foo')).to.equal('bar');
+      await storage.setItem("foo", "bar");
+      expect(await storage.getItem("foo")).to.equal("bar");
 
       const mockStore = getMockStore({}, storage);
       const store = mockStore({});
-      await store.dispatch(remove('foo'));
+      await store.dispatch(remove("foo"));
 
-      expect(await storage.getItem('foo')).to.equal(null);
+      expect(await storage.getItem("foo")).to.equal(null);
     });
 
-    it('should hydrate the redux store', async () => {
+    it("should hydrate the redux store", async () => {
       const storage = getAsyncStorage();
-      await storage.setItem('foo', JSON.stringify({ key: 'val' }));
+      await storage.setItem("foo", JSON.stringify({ key: "val" }));
 
-      const mockStore = getMockStore({
-        foo: {
-          load: ((value, { dispatch }) => {
-            dispatch({
-              type: 'hydrate',
-              value: JSON.parse(value),
-            });
-          }),
+      const mockStore = getMockStore(
+        {
+          foo: {
+            load: (value, { dispatch }) => {
+              dispatch({
+                type: "hydrate",
+                value: JSON.parse(value),
+              });
+            },
+          },
         },
-      }, storage);
+        storage,
+      );
 
       const store = mockStore({});
       await store.dispatch(hydrate());
 
-      expect(store.getActions()).to.deep.equal([{
-        type: 'hydrate',
-        value: { key: 'val' },
-      }]);
+      expect(store.getActions()).to.deep.equal([
+        {
+          type: "hydrate",
+          value: { key: "val" },
+        },
+      ]);
     });
   });
 });
